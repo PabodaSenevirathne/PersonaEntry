1ad6cea78439368119dca003cbcd9cccced5db09
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2023-11-21 13:29:48 -0500

    initial commit

---
+++ PersonaEntry/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonaEntry.Models;
+using System.Diagnostics;
+
+namespace PersonaEntry.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult PersonInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostPersonInfo(Person person)
+        {
+            // Save the person data or do whatever you need
+            // Redirect to a new page (PersonAdded) with the submitted data
+            return RedirectToAction("PersonAdded", person);
+        }
+
+        public IActionResult PersonAdded(Person person)
+        {
+            // Display the Person data
+            return View(person);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PersonaEntry/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonaEntry.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Province { get; set; }
+
+    }
+}
